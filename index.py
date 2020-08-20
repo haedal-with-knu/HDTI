d24fabb78d884f5f750a15ec0589c3c41c6186eb
Joo-ra-n <kjw995@gmail.com>
2020-08-20 16:00:09 +0900

    middle completE

--- index.py
+++ index.py
@@ -1,11 +1,16 @@
-from loadquestion import questions
-from flask import request, render_template, redirect, Flask
+from loadquestion import *
+from back import *
+from flask import render_template, redirect, Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', questions=questions)
+    return render_template('index.html', questions=questions, tracks=tracks)
+
+@app.route('/result')
+def result():
+    return render_template('result.html', img=img, type=type, explanation=explanation, track1=track1, track2=track2)
 
 if __name__ == '__main__':
     app.run(debug=1)
--- loadquestion.py
+++ loadquestion.py
@@ -1,13 +1,19 @@
 import csv
 
 # 질문을 배열에 넣어주자
-f = open('question.csv', 'r')
-questions =[]
+f = open('question.csv', 'r', encoding='utf-8')
+f2 = open('track.csv', 'r', encoding='utf-8')
+questions = []
+tracks = []
 
 rdr = csv.reader(f)
+rdr2 = csv.reader(f2)
+
 for line in rdr:
     # print(line)
     questions += line
-
 f.close()
 
+for line in rdr2:
+    tracks += line
+f.close()
