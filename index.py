02152f7ba28dcedafaf2bb996a1a0f8075ef2244
Joo-ra-n <kjw995@gmail.com>
2020-08-24 18:55:18 +0900

    fuck

--- index.py
+++ index.py
@@ -1,15 +1,16 @@
-from loadquestion import *
-from back import *
-from flask import render_template, redirect, Flask
+import loadquestion
+from flask import render_template,  Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', questions=questions, tracks=tracks)
 
-@app.route('/result')
+    return render_template('index.html', questions=loadquestion.questions, tracks=loadquestion.tracks)
+
+@app.route('/result', methods=["POST"])
 def result():
+    loadquestion.types
     return render_template('result.html', img=img, type=type, explanation=explanation, track1=track1, track2=track2)
 
 if __name__ == '__main__':
